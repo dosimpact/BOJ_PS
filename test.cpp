2b78b56db0c84dc9fa88db5a0d46473fe0023c1c
DosImpact <ypd03008@gmail.com>
2020-01-04 18:47:59 +0900

    14889_

--- test.cpp
+++ test.cpp
@@ -1,58 +1,62 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+
 using namespace std;
-int main()
+int s[20][20];
+int n;
+int go(int index, vector<int> &first, vector<int> &second)
 {
-    int n;
-    cin >> n;
-    vector<vector<int>> a(n, vector<int>(n));
-    for (int i = 0; i < n; i++)
+    if (index == n)
     {
-        for (int j = 0; j < n; j++)
-        {
-            cin >> a[i][j];
-        }
-    }
-    vector<int> b(n);
-    for (int i = 0; i < n / 2; i++)
-    {
-        b[i] = 1;
-    }
-    sort(b.begin(), b.end());
-    int ans = 2147483647;
-    do
-    {
-        vector<int> first, second;
-        for (int i = 0; i < n; i++)
-        {
-            if (b[i] == 0)
-            {
-                first.push_back(i);
-            }
-            else
-            {
-                second.push_back(i);
-            }
-        }
-        int one = 0;
-        int two = 0;
+        if (first.size() != n / 2)
+            return -1;
+        if (second.size() != n / 2)
+            return -1;
+        int t1 = 0;
+        int t2 = 0;
         for (int i = 0; i < n / 2; i++)
         {
             for (int j = 0; j < n / 2; j++)
             {
                 if (i == j)
                     continue;
-                one += a[first[i]][first[j]];
-                two += a[second[i]][second[j]];
+                t1 += s[first[i]][first[j]];
+                t2 += s[second[i]][second[j]];
             }
         }
-        int diff = one - two;
+        int diff = t1 - t2;
         if (diff < 0)
             diff = -diff;
-        if (ans > diff)
-            ans = diff;
-    } while (next_permutation(b.begin(), b.end()));
-    cout << ans << '\n';
-    return 0;
+        return diff;
+    }
+    int ans = -1;
+    first.push_back(index);
+    int t1 = go(index + 1, first, second);
+    if (ans == -1 || (t1 != -1 && ans > t1))
+    {
+        ans = t1;
+    }
+    first.pop_back();
+    second.push_back(index);
+    int t2 = go(index + 1, first, second);
+    if (ans == -1 || (t2 != -1 && ans > t2))
+    {
+        ans = t2;
+    }
+    second.pop_back();
+    return ans;
+}
+int main()
+{
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> s[i][j];
+        }
+    }
+    vector<int> first, second;
+    cout << go(0, first, second) << '\n';
 }

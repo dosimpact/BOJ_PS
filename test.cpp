7c1050066a7b609171d24142733fcc59a39e4ea5
DosImpact <ypd03008@gmail.com>
2020-01-23 19:36:53 +0900

    2206

--- test.cpp
+++ test.cpp
@@ -1,29 +1,15 @@
 #include <iostream>
 #include <queue>
-#include <deque>
-#include <vector>
+#include <cstdio>
+#include <tuple>
 using namespace std;
-bool c[1000000];
-int d[1000000];
-int MAX = 1000000;
+
 int main()
 {
-    vector<int> v;
-    v.push_back(1);
-    v.push_back(2);
-    v.push_back(3);
-    v.push_back(4);
-    v.push_back(5);
-    for (auto &k : v)
+    int ans1 = 3;
+    int ans2 = 4;
+    if (ans1 && ans2)
     {
-        cout << k << " "; //1 2 3 4 5
+        printf("%d", min(ans1, ans2));
     }
-    cout << "\n";
-    v.erase(v.begin(), v.begin() + 1);
-    for (auto &k : v)
-    {
-        cout << k << " "; //2 3 4 5
-    }
-    cout << "\n";
-    cout << v[0]; // 2
 }

99a7124cf9490aa7d081979fa3aaf2f91ed444b3
DosImpact <ypd03008@gmail.com>
2020-01-28 16:53:32 +0900

    9519

--- Silver/9519.cpp
+++ Silver/9519.cpp
@@ -1,6 +1,7 @@
 //https://www.acmicpc.net/problem/9519
 
 #include <iostream>
+#include <cstdio>
 #include <tuple>
 #include <queue>
 #include <algorithm>
@@ -8,47 +9,47 @@
 #include <vector>
 #include <string>
 #include <stack>
+#include <cmath>
+#define SIZE 51
 using namespace std;
-string next(string s)
-{
-    stack<char> st;
-    string ans;
-    for (int i = 0; i < s.size(); i++)
-    {
-        if (i % 2 == 0)
-        {
-            ans += s[i];
-        }
-        else
-        {
-            st.push(s[i]);
-        }
-    }
-    while (!st.empty())
-    {
-        ans += st.top();
-        st.pop();
-    }
-    return ans;
-}
+int graph[SIZE][SIZE];
+int n, m;
 int main()
 {
-    int x;
-    string s;
-    cin >> x >> s;
-    vector<string> data;
-    data.push_back(s);
-    while (true)
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
     {
-        s = next(s);
-        if (s != data[0])
+        for (int j = 0; j < m; j++)
         {
-            data.push_back(s);
-        }
-        else
-        {
-            break;
+            //cin >> graph[i][j];
+            scanf("%1d", &graph[i][j]);
         }
     }
-    cout << data[x % data.size()];
+    int ans_max = 1;
+    int span = min(n, m);
+    //전체 배열을 탐색하면서 | 행으로 + 1씩 해가면서 | 범위 체크 | 범위 안이라면 -> 마지막 꼭지점 체크 -> 정답 후보 넣기.
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+
+            for (int k = 1; k < span; k++)
+            {
+                int x = i + k;
+                int y = j + k;
+                if (x >= n || y >= m)
+                    continue;
+                if (graph[i][j] == graph[x][j] && graph[i][j] == graph[i][y] && graph[i][j] == graph[x][y])
+                {
+                    // printf("[%d,%d] : span %d", i, j, k);
+                    int tmp = pow(k + 1, 2);
+                    if (ans_max < tmp)
+                    {
+                        ans_max = tmp;
+                    }
+                }
+            }
+        }
+    }
+    cout << ans_max;
 }

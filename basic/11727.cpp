39ad0f4ee091b007208cf9f0269139166dfabe2e
DosImpact <ypd03008@gmail.com>
2020-01-28 17:12:22 +0900

    11727

--- basic/11727.cpp
+++ basic/11727.cpp
@@ -31,20 +31,9 @@ int dp(int n)
         return d[n];
     }
 }
-int dp2(int n)
-{
-    d[0] = 1;
-    d[1] = 1;
-    for (int i = 2; i <= n; i++)
-    {
-        d[i] = d[i - 1] + d[i - 2] * 2;
-        d[i] = d[i] % 10007;
-    }
-    return d[n];
-}
 int main()
 {
     int n;
     cin >> n;
-    cout << dp2(n);
+    cout << dp(n);
 }

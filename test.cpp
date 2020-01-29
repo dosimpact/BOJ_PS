32ad4466515bb187b5e0c84cd213d9fabfde47b5
DosImpact <ypd03008@gmail.com>
2020-01-29 16:41:25 +0900

    _5549

--- test.cpp
+++ test.cpp
@@ -1,15 +1,33 @@
+//https://www.acmicpc.net/problem/5549
+
 #include <iostream>
-#include <queue>
-#include <cstdio>
 #include <tuple>
+#include <queue>
+#include <algorithm>
+#include <deque>
+#include <vector>
+#include <string>
+#include <stack>
+#define SIZE 1001
 using namespace std;
+
+struct JOI
+{
+    int j;
+    int o;
+    int i;
+    JOI operator+(JOI &p)
+    {
+
+        return {j + p.j, o + p.o, i + p.i};
+    }
+};
+
+JOI d[SIZE][SIZE];
+int n, m, k;
+vector<string> graph;
 
 int main()
 {
-    int ans1 = 3;
-    int ans2 = 4;
-    if (ans1 && ans2)
-    {
-        printf("%d", min(ans1, ans2));
-    }
+    cout << d[0][0].j;
 }

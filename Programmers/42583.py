3cba715b08bef0b90d831a1882ec4a93d9a09386
DosImpact <ypd03008@gmail.com>
2020-02-17 18:29:52 +0900

    update file system

--- Programmers/42583.py
+++ Programmers/42583.py
@@ -3,21 +3,28 @@
 
 def solution(blen, w, trucks):  # 다리 길이 / 총 무게 / 트럭들 무게
     sec = 0
-    now_trucks = []  # [ 트럭의 무게 , 남은 트럭의 거리]
+    bridgeTrucks = []
     while True:
-        if len(now_trucks) != 0:
-            for i in range(len(now_trucks)):
-                now_trucks[i][1] -= 1
-        now_trucks = list(filter(lambda x: x[1] > 0, now_trucks))
-        # 트럭이 다 건넌경우 중단,
-        if len(trucks) == 0 and len(now_trucks) == 0:
+        # 트럭을 한칸씩 움직인다. | 0이 되버린 트럭은 제거 | 필터를 써서 제거함
+        for e in bridgeTrucks:
+            e[1] -= 1
+        bridgeTrucks = list(filter(lambda x: x[1] > 0, bridgeTrucks))
+
+        # 끝난경우 | 다리위 트럭 없음 | 더 갈 트럭도 없음.
+        if len(trucks) == 0 and len(bridgeTrucks) == 0:
             break
-        # 남은 트럭이 있는경우 -> 현재 건너는중인 트럭 무게 + 건너려는 트럭 무게 합 이 w 이하라면 추가
-        if(len(trucks) != 0):
-            tmpWeight = trucks[0]
-            for i in range(len(now_trucks)):
-                tmpWeight += now_trucks[i][0]
-            if tmpWeight <= w:
-                now_trucks.append([trucks.pop(0), blen])
+
+        # 트럭을 하나 추가해 볼까? | 트럭스가 있고 | 그 트럭무게 + 다리위 트럭무게함 | 되면 넣기
+        if len(trucks) > 0:
+            weight = trucks[0]
+            for e in bridgeTrucks:
+                weight += e[0]
+            if weight <= w:
+                bridgeTrucks.append([trucks.pop(0), blen])
         sec += 1
-    return sec + 1
+    return sec+1
+
+
+# 피드백 1 filter는 결과로 list가 아닌 filter 오브젝트를 반환한다.
+
+# 피드백 2 꼭 논리를 작성하고 코드 짜기.!!

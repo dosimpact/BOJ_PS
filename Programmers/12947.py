6c2a4b2b3f3162852189dd187f6ad143948d24fe
DosImpact <ypd03008@gmail.com>
2020-02-18 14:11:36 +0900

    12947

---
+++ Programmers/12947.py
@@ -0,0 +1,11 @@
+def solution(x):
+    answer = True
+    xlist = list(str(x))
+    sum = 0
+    for e in xlist:
+        sum += int(e)
+    if x % sum == 0:
+        answer = True
+    else:
+        answer = False
+    return answer
--- mainpy.py
+++ mainpy.py
@@ -3,11 +3,3 @@ import sys
 
 
 def input(): return sys.stdin.readline().rstrip()
-
-
-data = [[1, 1], [2, 3]]
-print(data)
-for e in data:
-    if e[1] == 1:
-        data.pop(e)
-print(data)

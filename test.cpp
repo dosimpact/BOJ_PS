1344d5a3508b8d7eeba3546498f2e8f20be6e9b5
DosImpact <ypd03008@gmail.com>
2020-02-22 14:40:07 +0900

    _2733

--- test.cpp
+++ test.cpp
@@ -7,46 +7,45 @@ using namespace std;
 int n, m, cnt = 0; //책의 개수, 한번에 들 수 있는 책의 수, 총 움직인 거리
 vector<int> v;     //책의 위치
 
-/*한번 움직일 때 m만큼 들고 이동. 
-책을 모두 제자리에 놓은 뒤 다시 시작지점으로 돌아올 필요 없음.
-그러므로 가장 먼 곳에 놓아야 하는 책은 왕복X */
-
 int main()
 {
-    cin >> n >> m; //n,m 입력
+    //입력
+    cin >> n >> m;
     for (int i = 0; i < n; i++)
     {
-        int a;
-        cin >> a;
-        v.push_back(a);
-    } //위치(좌표) 입력
-
-    sort(v.begin(), v.end()); //가장 작은 수가 왼쪽에, 가장 큰 수가 오른쪽에 오도록 정렬
-
-    if (abs(v.front()) >= abs(v.back())) //가장 먼 음수좌표의 거리가 가장 먼 양수좌표의 거리보다 멀 경우
+        int tmp;
+        cin >> tmp;
+        v.push_back(tmp);
+    }
+    // 한번에 m개책을 | 처음에는 한번만 가면됨
+    sort(v.begin(), v.end());
+    //음이 가장 큰 경우
+    if (abs(v.front()) >= abs(v.back()))
     {
-        cnt += abs(v.front());                            //가장 멀리 떨어진 음수좌표 거리만큼 이동 (왕복x)
-        for (int i = m; v[i] < 0 && i < v.size(); i += m) //음수좌표를 왔다갔다 할 때 반복
+        cnt += abs(v.front());
+        //음을 처리 ( 음과 양은 한번에 불가!)
+        for (int i = m; i < v.size() && v[i] < 0; i += m)
         {
             cnt += abs(v[i]) * 2;
         }
-        for (int i = v.size() - 1; v[i] > 0 && i >= 0; i -= m) //양수좌표를 왔다갔다 할 때 반복
-        {
-            cnt += v[i] * 2;
-        }
-    }
-    else //가장 먼 음수좌표의 거리가 가장 먼 양수좌표의 거리보다 멀지않을 때
-    {
-        cnt += abs(v.back());                             //가장 먼 거리 이동 (왕복x)
-        for (int i = 0; v[i] < 0 && i < v.size(); i += m) //음수좌표를 왔다갔다 할 때 반복
+        for (int i = v.size() - 1; i >= 0 && v[i] > 0; i -= m)
         {
             cnt += abs(v[i]) * 2;
         }
-        for (int i = (v.size() - 1) - m; v[i] > 0 && i >= 0; i -= m) //양수좌표를 왔다갔다 할 때 반복
+    }
+    //양이 가장 큰 경우
+    if (abs(v.front()) < abs(v.back()))
+    {
+        cnt += abs(v.back());
+        //음을 처리 ( 음과 양은 한번에 불가!)
+        for (int i = 0; i < v.size() && v[i] < 0; i += m)
         {
-            cnt += v[i] * 2;
+            cnt += abs(v[i]) * 2;
+        }
+        for (int i = v.size() - (m + 1); i >= 0 && v[i] > 0; i -= m)
+        {
+            cnt += abs(v[i]) * 2;
         }
     }
-    cout << cnt << endl; //출력
-    return 0;
+    cout << cnt;
 }
--- testpy.py
+++ testpy.py
@@ -4,48 +4,9 @@ import sys
 def input(): return sys.stdin.readline().rstrip()
 
 
-N, M = map(int, input().split())
-books = sorted(list(map(int, input().split())), reverse=True)
+data = ['[', '1', '2', '3', '[', '5', ']']
 
-didbs = list(filter(lambda x: x > 0, books))
-dmabs = sorted(
-    list(map(lambda x: abs(x), filter(lambda x: x < 0, books))), reverse=True)
+print(data[::])
 
-
-ans = 0
-
-
-def popbook(book, w):
-    global ans
-    ans += max(book)*w
-    book.pop(0)
-    if len(book) != 0:
-        book.pop(0)
-
-
-# 양북만 있는 경우 | 음북만 있는 경우 | 둘다 있는 경우
-if len(didbs) != 0 and len(dmabs) == 0:
-    popbook(didbs, 1)
-    while(len(didbs) != 0):
-        popbook(didbs, 2)
-
-elif len(didbs) == 0 and len(dmabs) != 0:
-    popbook(dmabs, 1)
-    while(len(dmabs) != 0):
-        popbook(dmabs, 2)
-
-elif len(didbs) != 0 and len(dmabs) != 0 and max(didbs) >= max(dmabs):
-    popbook(didbs, 1)
-    while(len(didbs) != 0):
-        popbook(didbs, 2)
-    while(len(dmabs) != 0):
-        popbook(dmabs, 2)
-
-elif len(didbs) != 0 and len(dmabs) != 0 and max(didbs) < max(dmabs):
-    popbook(dmabs, 1)
-    while(len(didbs) != 0):
-        popbook(didbs, 2)
-    while(len(dmabs) != 0):
-        popbook(dmabs, 2)
-
-print(ans)
+print(4+data[4::1].index(']'))
+print(6 - data[6::-1].index('['))

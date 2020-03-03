25f6a17a2529a865ac7ec47bfce8d4522b41d861
DosImpact <ypd03008@gmail.com>
2020-03-04 00:41:00 +0900

    make Math.md and modifier

--- mainpy.py
+++ mainpy.py
@@ -5,11 +5,8 @@ import sys
 def input(): return sys.stdin.readline().rstrip()
 
 
-def solution(arr):
-    arr.pop(arr.index(min(arr)))
-    if len(arr) == 0:
-        arr.append(-1)
-    return arr
-
-
-print(solution("1234"))
+(A, B, C) = map(int, input().split())
+print((A+B) % C)
+print((A % C + B % C) % C)
+print((A*B) % C)
+print((A % C * B % C) % C)

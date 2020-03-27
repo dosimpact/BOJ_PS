737fb63f5d68a294d89956b751887304717635e4
DosImpact <ypd03008@gmail.com>
2020-03-27 20:40:01 +0900

    3085

--- snippet.py
+++ snippet.py
@@ -1,1 +1,45 @@
-print(int(5**(1/2)))
+
+N = 5
+"""
+3
+YZZ
+ZYZ
+ZZY
+[['Y', 'C', 'P', 'Z', 'Y'], 
+['C', 'Y', 'Z', 'Z', 'P'], 
+['C', 'C', 'P', 'P', 'P'], 
+['C', 'Y', 'Y', 'Z', 'C'], 
+['C', 'P', 'P', 'Z', 'Z']]
+"""
+
+
+def maxVal():
+    graph = [['Y', 'C', 'P', 'Z', 'Y'], ['C', 'Y', 'Z', 'Z', 'P'], [
+        'C', 'C', 'P', 'P', 'P'], ['C', 'Y', 'Y', 'Z', 'C'], ['C', 'P', 'P', 'Z', 'Z']]
+    ans = 0
+    # 가장 긴 연속 부분(행 또는 열)을
+    for i in range(N):
+        tmp = 1
+        for j in range(N-1):  # 0 1 2 3      [][][][][]
+            if graph[i][j] == graph[i][j+1]:
+                tmp += 1
+            else:
+                ans = max(ans, tmp)
+                tmp = 1
+            print("->", graph[i][j], graph[i][j+1], "->", ans)
+
+    print(ans)
+    for j in range(N):  # 0
+        tmp = 1
+        for i in range(N-1):  # 0 1 2 3
+            if graph[i][j] == graph[i+1][j]:
+                tmp += 1
+            else:
+                ans = max(ans, tmp)
+                tmp = 1
+        ans = max(ans, tmp)
+    print(ans)
+    return ans
+
+
+maxVal()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,23 +1,4 @@
-import sys
-
-
-def input(): return sys.stdin.readline().rstrip()
-
-
-DEBUG = True
-
-N = int(input())
-inps = list(map(int, input().split()))
-
-ans_max = []
-for i in range(100000, -1, -1):  # i개의 말이 참일때
-    if inps.count(i) == i:
-        print(i)
-        exit(0)
-print(-1)
-
-# if ans_max[0] == 0:
-#     print(-1)
-# else:
-#     print(ans_max[0])
-# 가정을 하는거야, i개의 말이 참일때, i개의 말이 참이라는 명제를 말한것들의 갯수를 구해 -> 그게 i랑 동일하면 넣어.
+3
+YZZ
+ZYZ
+ZZY

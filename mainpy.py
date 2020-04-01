e59892ce1456c48c42485240d2914e377fc2ff70
DosImpact <ypd03008@gmail.com>
2020-04-01 22:12:57 +0900

    yes

--- mainpy.py
+++ mainpy.py
@@ -4,20 +4,25 @@ https://www.acmicpc.net/problem/2231
 """
 
 
+import math
 import sys
 
 
 def input(): return sys.stdin.readline().rstrip()
 
 
-n = int(input())
+X, Y = map(int, input().split())
 
-for k in range(1, n):
-    now = k
-    tmp = list(str(k))
-    for e in tmp:
-        now += int(e)
-    if n == now:
-        print(k)
-        exit(0)
-print(0)
+Z = int((Y/X)*100)
+
+# Z+1 이 목표
+
+dZ = (((Y+1)/(X+1))*100) - (((Y)/(X))*100)
+#print("->", X, Y, Z, dZ)
+a = dZ
+
+if Z < 99 and round(a, 9) != 0:
+    # 가능성
+    print(math.ceil(1/a))
+else:
+    print(-1)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,20 +1,28 @@
-6 8  5 3  5 2  6 4
-5 6  0 0
+"""
 
-8 1  7 3  6 2  8 9  7 5
-7 4  7 8  7 6  0 0
+https://www.acmicpc.net/problem/2231
+"""
 
-3 8  6 8  6 4
-5 3  5 6  5 2  0 0
 
-1 2  1 3  0 0
+import math
+import sys
 
-2 1  3 1  0 0
 
-1 2  2 3  3 1  4 5  0 0
+def input(): return sys.stdin.readline().rstrip()
 
-4 5 0 0
 
-1 2 2 3 3 1 0 0
+X, Y = map(int, input().split())
 
--1 -1
+Z = int((Y/X)*100)
+
+# Z+1 이 목표
+
+dZ = (((Y+1)/(X+1))*100) - (((Y)/(X))*100)
+#print("->", X, Y, Z, dZ)
+a = dZ
+
+if Z != 99 and round(a, 9) != 0:
+    # 가능성
+    print(math.ceil(1/a))
+else:
+    print(-1)

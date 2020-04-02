34e57d0180ebe1bd1405f80d87b185fd8a14c591
DosImpact <ypd03008@gmail.com>
2020-04-02 11:53:18 +0900

    5618

--- mainpy3.py
+++ mainpy3.py
@@ -1,80 +1,26 @@
 import sys
+import functools
+from itertools import permutations, combinations
 
 
 def input(): return sys.stdin.readline().rstrip()
 
 
-N = int(input())
+Debug = True
 
-graph = [list(input()) for _ in range(N)]
-
-
-# XX..A..CCC...BBB
+"""
 
 
-def isVaild(s: []):
-    cnt = 0
-    now = "."
-    # .인경우는 무조건 초기화
-    for i in range(len(s)):
-        # .이 아닌 경우, 같은거 또 나온겨우
-        if s[i] == '.':
-            cnt = 0
-            now = '.'
+"""
+n = int(input())
+sus = list(map(int, input().split()))
+
+for i in range(1, max(sus)+1):
+    isposs = True
+    for su in sus:
+        if su % i == 0:
+            pass
         else:
-            if now == s[i]:
-                cnt += 1
-                if cnt >= 3:
-                    return now
-            else:
-                now = s[i]
-                cnt = 1
-
-                # 다른게 나온경우. ( . 에서 새롭게 나온경우)
-    return '.'
-
-
-# 가로 탐색
-for i in range(N):
-    tmp = isVaild(graph[i])
-    if(tmp != '.'):
-        print(tmp)
-        exit(0)
-
-# 세로 탐색
-for j in range(N):
-    rcd = [graph[i][j] for i in range(N)]
-    tmp = isVaild(rcd)
-    if(tmp != '.'):
-        print(tmp)
-        exit(0)
-
-# 대각선 오아 탬색
-for i in range(N):
-    for j in range(N):
-        if i == 0 or j == 0:
-            rcd = [graph[i+k][j+k]
-                   for k in range(N) if (i+k) < N and (j+k) < N]
-            tmp = isVaild(rcd)
-            if(tmp != '.'):
-                print(tmp)
-                exit(0)
-
-# 대각선 왼아 탐색
-for i in range(N):
-    for j in range(N):
-        if i == 0 or j == N-1:
-            rcd = [graph[i+k][j-k]
-                   for k in range(N) if (i+k) < N and 0 <= (j-k)]
-            tmp = isVaild(rcd)
-            if(tmp != '.'):
-                print(tmp)
-                exit(0)
-
-print("ongoing")
-"""
-3
-XXT
-XTC
-T..
-"""
+            isposs = False
+    if isposs:
+        print(i)

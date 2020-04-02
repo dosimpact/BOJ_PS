c9fc6f0266eeea58f5eb6ec8055e118421626597
DosImpact <ypd03008@gmail.com>
2020-04-03 00:09:35 +0900

    60058

--- snippet.py
+++ snippet.py
@@ -1,27 +1,14 @@
-
-# XX..A..CCC...BBB
-
-
-def isVaild(s: []):
+def diver(s: str):
     cnt = 0
-    now = "."
-    # .인경우는 무조건 초기화
-    for i in range(len(s)):
-        # .이 아닌 경우, 같은거 또 나온겨우
-        if s[i] == '.':
-            cnt = 0
-            now = '.'
+    idx = 0
+    for i, se in enumerate(s):
+        if se == '(':
+            cnt += 1
         else:
-            if now == s[i]:
-                cnt += 1
-                if cnt >= 3:
-                    return now
-            else:
-                now = s[i]
-                cnt = 1
-
-                # 다른게 나온경우. ( . 에서 새롭게 나온경우)
-    return '.'
+            cnt -= 1
+        if cnt == 0:
+            return (s[:i+1], s[i+1:])
+    return "ERROR"
 
 
-print(isVaild("KKYK.....XX..A..CCBC...BB.TTT"))
+print(diver("()))((()"))

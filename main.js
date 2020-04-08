6c9bce59587d3102dfdb55c0cf825cfb0ecfc81c
DosImpact <ypd03008@gmail.com>
2020-04-08 11:46:40 +0900

    cafe

--- main.js
+++ main.js
@@ -1,10 +1,40 @@
-var a = 0;
-
-(function () {
-  this.a = 1;
-  this.b = 2;
-  console.log(a);
-})();
-
-console.log(a);
-console.log(b);
+class ObjectUtilities {
+  /* Your magic here */
+  static mergeObjects(...args) {
+    //const newObj = { ...A, ...B };
+    return args.reduce((p, c) => {
+      return { ...p, ...c };
+    });
+  }
+  static removePassword(A) {
+    const B = { ...A };
+    delete B.password;
+    return B;
+  }
+  static freezeObj(A) {
+    return Object.freeze(A);
+  }
+  static getOnlyValues(A) {
+    return Object.values(A);
+  }
+  static getOnlyProperties(A) {
+    return Object.keys(A);
+  }
+}
+const objA = {
+  name: "Nicolas",
+  favFood: "Kimchi",
+};
+const objB = {
+  password: "12345",
+};
+const user = ObjectUtilities.mergeObjects(objA, objB);
+console.log(user);
+const cleanUser = ObjectUtilities.removePassword(user);
+console.log(cleanUser);
+const frozenUser = ObjectUtilities.freezeObj(cleanUser);
+const onlyValues = ObjectUtilities.getOnlyValues(frozenUser);
+console.log(onlyValues);
+const onlyProperties = ObjectUtilities.getOnlyProperties(frozenUser);
+console.log(onlyProperties);
+frozenUser.name = "Hello!"; // This should show an error
--- mainpy2.py
+++ mainpy2.py
@@ -1,85 +1,71 @@
-from collections import defaultdict
+from collections import deque
+
+# 1초동안 움직일 수 있는 모든 경우
 
 
-class Node(object):
-    def __init__(self, key):
-        self.key = key
-        self.data = None
-        self.nexts = {}
-        self.lengths = defaultdict(int)
+def move(cor1, cor2, board):
+    move = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    ret = []
+    # 이동 | 이동했을때, 0이라면 이동한다.
+    for m in move:
+        if board[cor1[0]+m[0]][cor1[1]+m[1]] == 0 and board[cor2[0]+m[0]][cor2[1]+m[1]] == 0:
+            ret.append({(cor1[0]+m[0], cor1[1]+m[1]),
+                        (cor2[0]+m[0], cor2[1]+m[1])})
+
+    rotate = [1, -1]
+    # 가로회전 ( ㅡ  > |  ) | 아래로 회전 2경우 ( 아래 둘다 0 | 추가 2번 )
+    if cor1[0] == cor2[0]:
+        for r in rotate:
+            if board[cor1[0]+r][cor1[1]] == 0 and board[cor2[0]+r][cor2[1]] == 0:
+                ret.append({(cor1[0]+r, cor1[1]), (cor1[0], cor1[1])})
+                ret.append({(cor2[0]+r, cor2[1]), (cor2[0], cor2[1])})
+    # 세로회전 ( | -> ㅡ ) | 위로 회전하는 2경 ( 위 둘다 0 | 추가 2번)
+    else:
+        for r in rotate:
+            if board[cor1[0]][cor1[1]+r] == 0 and board[cor2[0]][cor2[1]+r] == 0:
+                ret.append({(cor1[0], cor1[1]), (cor1[0], cor1[1]+r)})
+                ret.append({(cor2[0], cor2[1]),  (cor2[0], cor2[1]+r)})
+    return ret
 
 
-class Trie(object):
-    def __init__(self):
-        self.head = Node(None)
+def solution(board):
+    size = len(board)
+    # 경계 체크 쉽게하기 위해서 지도의 상하좌우에 1 추가
+    new_board = [[1 for i in range(len(board)+2)] for i in range(len(board)+2)]
+    for i in range(len(board)):
+        for j in range(len(board)):
+            new_board[i+1][j+1] = board[i][j]
+    print(new_board)
+    que = deque()
+    visited = []
 
-    def insert(self, string):
-        curNode = self.head
-        curNode.lengths[len(string)] += 1
-        for s in string:
-            if s not in curNode.nexts:
-                curNode.nexts[s] = Node(s)
-            curNode = curNode.nexts[s]
-            curNode.lengths[len(string)] += 1
-        curNode.data = string
+    # queue에 [로봇의 좌표정보, 지금까지 거리] 형태로 넣음
+    que.append([{(1, 1), (1, 2)}, 0])
+    visited.append({(1, 1), (1, 2)})  # 방문을 set형태로 넣네, 1단위 인가?
 
-    def search(self, string):
-        curNode = self.head
-        for s in string:
-            if s in curNode.nexts:
-                curNode = curNode.nexts[s]
-            else:
-                return False
-        if curNode.data != None:
-            return True
-        else:
-            return False
+    while len(que) != 0:
+        temp = que.popleft()
+        cor = list(temp[0])  # 한단위 중 첫번쨰에 위치가 있다. , 두번쨰는 거리
+        dist = temp[1]+1
 
-    def starts_with(self, prefix):
-        curNode = self.head
-        for s in prefix:
-            if s in curNode.nexts:
-                curNode = curNode.nexts[s]
-            else:
-                return None
-        q = [curNode]
-        res = []
-        while q:
-            nnode = q.pop()
-            if nnode.data != None:
-                res.append(nnode.data)
-            q += list(nnode.nexts.values())
+        # 1유닛, 2유닛, 보드 > 해당 로봇의 상태에서 나올 수 있는 위치들을 모두 뽑아.
+        # [ { (유닛1) (유닛2)  } ]  # 로봇의 위치는 상관없는듯
+        for m in move(cor[0], cor[1], new_board):
+            if (size, size) in m:  # 정답인 경우
+                return dist
 
-        return res
+            if not m in visited:  # 해당 로봇의 조합을 안해본 경우.
+                que.append([m, dist])
+                visited.append(m)
 
-    def starts_withLen(self, prefix, L: int):
-        curNode = self.head
-        for s in prefix:
-            if s in curNode.nexts:
-                curNode = curNode.nexts[s]
-            else:
-                return 0
-
-        return curNode.lengths[L]
+    return 0
 
 
-def solution(words, queries):
-    answer = []
-    frontTree = Trie()
-    backTree = Trie()
-    for word in words:
-        frontTree.insert(word)
-        backTree.insert(word[::-1])
+print(solution([[0, 0, 0, 1, 1], [0, 0, 0, 1, 0], [
+      0, 1, 0, 1, 1], [1, 1, 0, 0, 1], [0, 0, 0, 0, 0]]))
 
-    for word in queries:
-        if word == "?"*len(word):
-            answer.append(frontTree.head.lengths[len(word)])
-        elif word[0] == "?":
-            prefix = word[::-1].split("?")[0]
-            res = backTree.starts_withLen(prefix, len(word))
-            answer.append(res)
-        elif word[-1] == "?":
-            res = frontTree.starts_withLen(word.replace("?", ""), len(word))
-            answer.append(res)
+"""
+fb)
+로봇이 두칸을 차지하지만, 1개의 노드로 봐야함.
 
-    return answer
+"""

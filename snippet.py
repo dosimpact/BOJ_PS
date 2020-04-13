5ab18240f073f3631e7324642fb8cf3b2cbaae72
DosImpact <ypd03008@gmail.com>
2020-04-13 14:20:00 +0900

    2804

--- snippet.py
+++ snippet.py
@@ -1,29 +1,4 @@
-wordNum = input()
-word = []
-for i in range(int(wordNum)):
-    temp = input()
-    word.append(list(temp))
-number = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
 
-# word: AAA AAB
+data = 123.127
 
-charDic = {}
-for i in range(len(word)):
-    for j in range(len(word[i])):
-        if word[i][j] not in charDic:
-            charDic[word[i][j]] = pow(10, len(word[i])-j-1)
-        else:
-            charDic[word[i][j]] += pow(10, len(word[i])-j-1)
-
-keyList = list(charDic.keys())
-for i in range(len(keyList)-1):
-    big = i
-    for j in range(len(keyList)-i-1):
-        if charDic[keyList[j]] < charDic[keyList[j+1]]:
-            keyList[j], keyList[j+1] = keyList[j+1], keyList[j]
-
-result = 0
-for i in keyList:
-    result += number.pop(0) * charDic[i]
-
-print(result)
+print(round(data, 2))

0350ab24af9695e2f87cf8351a8cb7e980f095a4
DosImpact <ypd03008@gmail.com>
2020-04-24 20:45:34 +0900

    이중우선순위큐

--- mainpy3.py
+++ mainpy3.py
@@ -1,26 +1,16 @@
-
-import heapq
-
-
 def solution(n, works: []):  # 무조건 최고의 workㄹ르 줄여라
     answer = 0
-    newWorks = list(map(lambda e: -e, works))
-    heapq.heapify(newWorks)
-    print(newWorks)
 
     for _ in range(n):
-
-        if newWorks[0] == 0:
-            return 0
+        idx = works.index(max(works))
+        if(works[idx] > 0):
+            works[idx] -= 1
         else:
-            w = heapq.heappop(newWorks)
-            heapq.heappush(newWorks, w+1)
-
-    # print(works)
-    for work in newWorks:
-        answer += abs(work)**2
+            break
+    print(works)
+    for work in works:
+        answer += work**2
     return answer
 
 
-print(solution(4, [4, 3, 3]	))
 print(solution(3, [1, 1]))

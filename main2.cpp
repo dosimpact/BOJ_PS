77bf9ad3d9b10f5b6967e6de9d466ec7f4d0d91d
DosImpact <ypd03008@gmail.com>
2020-05-11 14:16:27 +0900

    2869

--- main2.cpp
+++ main2.cpp
@@ -1,102 +1,285 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 
+#define BUFFER_SIZE 1000
+#define MAX_STRING_LENGTH 30
+#define ASCII_SIZE 256
 
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <string>
+pthread_cond_t thread_cond_producer = PTHREAD_COND_INITIALIZER;
+pthread_cond_t thread_cond_consumer = PTHREAD_COND_INITIALIZER;
 
-using namespace std;
-int n;
-int ans[10];
-int sign[10][10];
-
-bool check(int idx)
+typedef struct sharedobject
 {
-    int now = ans[idx];
-    for (int i = idx - 1; i >= 0; i--)
+    FILE *rfile;
+    int linenum;
+    char *lineQ[BUFFER_SIZE]; // stack Buffer
+    int Sidx;                 // stack buffer idxer can READ [0,100)
+    pthread_mutex_t lock;
+    int fin; // fin = 0 > 1 end read file
+    //int alphas['z' - 'a' + 1];
+    int stat[MAX_STRING_LENGTH];
+    int stat2[ASCII_SIZE];
+} so_t;
+
+void printAZ(int *stat2)
+{
+    printf("       A        B        C        D        E        F        G        H        I        J        K        L        M        N        O        P        Q        R        S        T        U        V        W        X        Y        Z\n");
+    printf("%8d %8d %8d %8d %8d %8d %8d %8d %8d %8d %8d %8d %8d %8d %8d %8d %8d %8d %8d %8d %8d %8d %8d %8d %8d %8d\n",
+           stat2['A'] + stat2['a'], stat2['B'] + stat2['b'], stat2['C'] + stat2['c'], stat2['D'] + stat2['d'], stat2['E'] + stat2['e'],
+           stat2['F'] + stat2['f'], stat2['G'] + stat2['g'], stat2['H'] + stat2['h'], stat2['I'] + stat2['i'], stat2['J'] + stat2['j'],
+           stat2['K'] + stat2['k'], stat2['L'] + stat2['l'], stat2['M'] + stat2['m'], stat2['N'] + stat2['n'], stat2['O'] + stat2['o'],
+           stat2['P'] + stat2['p'], stat2['Q'] + stat2['q'], stat2['R'] + stat2['r'], stat2['S'] + stat2['s'], stat2['T'] + stat2['t'],
+           stat2['U'] + stat2['u'], stat2['V'] + stat2['v'], stat2['W'] + stat2['w'], stat2['X'] + stat2['x'], stat2['Y'] + stat2['y'],
+           stat2['Z'] + stat2['z']);
+}
+void *producer(void *arg)
+{
+
+    so_t *so = arg;
+    int *ret = malloc(sizeof(int));
+    FILE *rfile = so->rfile;
+    int i = 0;
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read = 0;
+    //------------------------------------------init setting
+    pthread_mutex_lock(&so->lock);
+    while (1)
     {
-        now += ans[i];
-        if (sign[i][idx] == 0)
+        //데이터를 더 채울 수 있는 경우  // Read More Case
+        if (so->Sidx < BUFFER_SIZE && so->fin == 0)
         {
-            if (now != 0)
-                return false;
-        }
-        else if (sign[i][idx] == -1)
-        {
-            if (now >= 0)
+            read = getdelim(&line, &len, '\n', rfile);
+            if (read == -1)
             {
-                return false;
+                printf("producer : READING complete! \n");
+                so->fin = 1;
+                //pthread_cond_broadcast(&thread_cond_producer);
+                pthread_cond_broadcast(&thread_cond_consumer);
+                break;
             }
+            if (so->Sidx == -1)
+            {
+                so->Sidx = 0;
+            }
+            so->linenum = i;
+            so->lineQ[so->Sidx] = strdup(line); /* share the line */
+            printf("line : %d is upload complete\n", so->linenum);
+            i++;
+            so->Sidx++;
+            pthread_cond_broadcast(&thread_cond_consumer);
+        }
+        else if (so->Sidx >= BUFFER_SIZE && so->fin == 0)
+        { //데이터가 소비가 안된경우 //Sleep Case
+            printf("producer sleep\n");
+            pthread_cond_wait(&thread_cond_producer, &so->lock);
+            pthread_cond_broadcast(&thread_cond_consumer);
+        }
+        else if (so->fin == 1)
+        { //데이터를 다 읽은 경우 //End case
+            //pthread_cond_broadcast(&thread_cond_producer); // when multi producer, wake up others!
+            pthread_cond_broadcast(&thread_cond_consumer);
         }
         else
         {
-            if (now <= 0)
-                return false;
+            printf("ERROR!\n");
         }
     }
-    return true;
+    pthread_mutex_unlock(&so->lock);
+    free(line);
+    printf("Prod_%x: %d lines\n", (unsigned int)pthread_self(), i);
+    *ret = i;
+    pthread_exit(ret);
 }
 
-bool go(int idx) // 리턴
+void *consumer(void *arg)
 {
-    //현재 인덱스가 n까지 도달 = 성공
-    if (idx == n)
+    so_t *so = arg;
+    int *ret = malloc(sizeof(int));
+    int i = 0;
+    int len;
+    char *line = NULL;
+    //------------------------------------------init setting
+    //int alphas['z' - 'a' + 1];
+    size_t length = 0;
+    int stat[MAX_STRING_LENGTH];
+    int stat2[ASCII_SIZE];
+    memset(stat, 0, sizeof(stat));
+    memset(stat2, 0, sizeof(stat));
+    pthread_mutex_lock(&so->lock);
+    while (1)
     {
-        return true;
-    }
-    if (sign[idx][idx] == 0)
-    {
-        ans[idx] = 0;
-        return check(idx) && go(idx + 1);
-    }
-    for (int i = 1; i <= 10; i++)
-    {
-        ans[idx] = sign[idx][idx] * i;
-        if (check(idx) && go(idx + 1))
-            return true;
-    }
-    return false;
-    //현재 부호를 보고 0이면 0 넣보고, 검사 해보고 go
-}
-
-int main(void)
-{
-    //입력 받기
-    cin >> n;
-    string s;
-    cin >> s;
-    int cnt = 0;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = i; j < n; j++)
+        // 데이터를 더 읽을 수 있는 경우// Consume Case || 다 읽으면 자버리기 Sidx -1, 100
+        if ((0 <= so->Sidx && so->Sidx <= BUFFER_SIZE))
         {
-            if (s[cnt] == '+')
+            so->Sidx--;
+            if (so->Sidx <= -1)
             {
-                sign[i][j] = 1;
+                continue;
             }
-            if (s[cnt] == '-')
+            line = so->lineQ[so->Sidx];
+            so->lineQ[so->Sidx] = NULL;
+            len = strlen(line);
+            printf("Cons_%x: [%02d:%02d] %s",
+                   (unsigned int)pthread_self(), i, so->linenum, line);
+            int j = 0;
+            for (j = 0; line[j]; j++)
             {
-                sign[i][j] = -1;
+                stat2[line[j]]++;
             }
-            if (s[cnt] == '0')
+            length = strlen(line);
+            if (length >= 30)
+                length = 30;
+            if (length <= 0)
+                length = 1;
+            stat[length - 1]++;
+            i++;
+            free(line);
+            if (so->Sidx <= -1)
             {
-                sign[i][j] = 0;
+                continue;
             }
-            cnt++;
+        }
+        else if (so->fin == 0 && (so->Sidx <= -1 || so->Sidx > BUFFER_SIZE)) // 끝나지 않았지만, 읽을데이터가 없는 경우 // Sleep case
+        {
+            printf("consumer sleep\n");
+            pthread_cond_broadcast(&thread_cond_producer);
+            pthread_cond_wait(&thread_cond_consumer, &so->lock);
+        }
+        else if (so->fin == 1) //End case
+        {
+            pthread_cond_broadcast(&thread_cond_consumer);
+            pthread_cond_broadcast(&thread_cond_producer);
+            break;
         }
     }
-    // for (int i = 0; i < n; i++)
+
+    printf("Cons: %d lines\n", i);
+    // for (int it = 0; it < ('z' - 'a' + 1); it++)
     // {
-    //     for (int j = i; j < n; j++)
-    //     {
-    //         cout << sign[i][j] << "  ";
-    //     }
-    //     cout << "\n";
+    //     printf("[%c] : %d ", 'a' + it, alphas[it]);
+    //     so->alphas[it] += alphas[it];
     // }
-    //go(0)돌기
-    go(0);
-    for (int i = 0; i < n; i++)
+    // state share
+    printAZ(stat2);
+    for (int it = 0; it < MAX_STRING_LENGTH; it++)
     {
-        cout << ans[i] << " ";
+        so->stat[it] += stat[it];
     }
+    for (int it = 0; it < ASCII_SIZE; it++)
+    {
+        so->stat2[it] += stat2[it];
+    }
+    //printf("\n");
+    pthread_mutex_unlock(&so->lock);
+    *ret = i;
+    pthread_exit(ret);
+}
+
+int main(int argc, char *argv[])
+{
+    pthread_t prod[100];
+    pthread_t cons[100];
+    int Nprod, Ncons;
+    int rc;
+    long t;
+    int *ret;
+    int i;
+    FILE *rfile;
+    if (argc == 1)
+    {
+        printf("usage: ./prod_cons <readfile> #Producer #Consumer\n");
+        exit(0);
+    }
+    so_t *share = malloc(sizeof(so_t));
+    memset(share, 0, sizeof(so_t));
+    rfile = fopen((char *)argv[1], "r");
+    if (rfile == NULL)
+    {
+        perror("rfile");
+        exit(0);
+    }
+    if (argv[2] != NULL)
+    {
+        Nprod = atoi(argv[2]);
+        if (Nprod > 100)
+            Nprod = 100;
+        if (Nprod == 0)
+            Nprod = 1;
+    }
+    else
+        Nprod = 1;
+    if (argv[3] != NULL)
+    {
+        Ncons = atoi(argv[3]);
+        if (Ncons > 100)
+            Ncons = 100;
+        if (Ncons == 0)
+            Ncons = 1;
+    }
+    else
+        Ncons = 1;
+
+    share->rfile = rfile;
+    share->Sidx = -1;
+    pthread_mutex_init(&share->lock, NULL);
+    for (i = 0; i < Nprod; i++)
+    {
+        int res = pthread_create(&prod[i], NULL, producer, share);
+        if (res < 0)
+        {
+            perror("thread create error");
+        }
+    }
+    usleep(10);
+    for (i = 0; i < Ncons; i++)
+    {
+        int res = pthread_create(&cons[i], NULL, consumer, share);
+        if (res < 0)
+        {
+            perror("thread create error");
+        }
+    }
+
+    printf("main continuing\n");
+
+    for (i = 0; i < Ncons; i++)
+    {
+        rc = pthread_join(cons[i], (void **)&ret);
+        printf("main: consumer_%d joined with %d\n", i, *ret);
+    }
+    for (i = 0; i < Nprod; i++)
+    {
+        rc = pthread_join(prod[i], (void **)&ret);
+        printf("main: producer_%d joined with %d\n", i, *ret);
+    }
+    printf("-------------- final A-Z counter ---------------\n");
+    // for (int it = 0; it < ('z' - 'a' + 1); it++)
+    // {
+    //     printf("[%c] : %d ", 'a' + it, share->alphas[it]);
+    // }
+    // sum
+    int sum = 0;
+    for (i = 0; i < 30; i++)
+    {
+        sum += share->stat[i];
+    }
+    // print out distributions
+    printf("*** print out distributions *** \n");
+    for (i = 0; i < 30; i++)
+    {
+        int j = 0;
+        int num_star = share->stat[i] * 80 / sum;
+        printf("[lineLen : %3d]: %4d \t", i + 1, share->stat[i]);
+        for (j = 0; j < num_star; j++)
+            printf("*");
+        printf("\n");
+    }
+    printAZ(share->stat2);
+    printf("\n");
+    pthread_exit(NULL);
+    exit(0);
 }
--- mainpy4.py
+++ mainpy4.py
@@ -1,4 +1,5 @@
 import sys
+from copy import deepcopy
 
 
 def input(): return sys.stdin.readline().rstrip()
@@ -6,21 +7,38 @@ def input(): return sys.stdin.readline().rstrip()
 
 sys.setrecursionlimit(10**6)
 
-ansList = []
+q = []  # 작업 큐
+plist = [[10000, 0, 0, 1], [150000, 0, 0, 2], [100000, 0, 0, 3], [
+    8000, 0, 0, 4], [7000, 0, 0, 5]]  # 실행 되어야 할 프로세스 시간 (ms) 대기시간, 종료된 시간
+reslist = []
+quantum = 5000  # CASE1. 퀀텀이 5s인 경우
 
+clk = 0  # 전체 시간
+qtimer = 0  # 퀀텀 타이머
 
-def hanoi(fr, by, to, n: int):
-    global ansList
-    if n == 1:
-        ansList.append((fr, to))
-        return
-    hanoi(fr, to, by, n-1)
-    ansList.append((fr, to))
-    hanoi(by, fr, to, n-1)
-
-
-N = int(input())
-hanoi(1, 2, 3, N)
-print(len(ansList))
-for al in ansList:
-    print(*al)
+q = deepcopy(plist)  # 작업큐에 모든 프로세스가 들어온다.
+# ========================================================================init
+print(f"작업 큐 : {q}")
+while q:  # 작업큐가 없을때 까지.
+    # 해당 ms에서 작업을 실행하고
+    q[0][0] -= 1
+    # 각 프로세스에 대한, 대기시간과 실행중인 프로세스작업을 업데이트
+    for i in range(1, len(q), 1):
+        q[i][1] += 1
+    clk += 1
+    qtimer += 1
+    # 만약 작업이 종료되면 | 종료시간 적고,다음 작업 큐로 전환
+    if q[0][0] <= 0:
+        print(f"finish process at {clk}")
+        q[0][2] = clk
+        tmp = q.pop(0)
+        reslist.append(tmp)
+        continue
+    # 퀀텀이 종료되면 | 다시 스케쥴링
+    if qtimer >= quantum:
+        tmp = q.pop(0)
+        q.append(tmp)
+        qtimer = 0
+        print(f"swtich process at {clk}")
+        continue
+print(f"result : {reslist}")

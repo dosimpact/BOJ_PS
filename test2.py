1bb721091776b05cc993b770dfd9853db391c39a
DosImpact <ypd03008@gmail.com>
2020-05-13 13:49:12 +0900

    1874

--- test2.py
+++ test2.py
@@ -1,17 +1,44 @@
+from copy import deepcopy
 import sys
 import math
 
 
 def input(): return sys.stdin.readline().rstrip()
 
-"""
-2 // T
-6 12 10 // H W N
-30 50 72 // H W N
-"""
 
-a, b, v = map(int, input().split())
+N = int(input())
+wntList = []
+for _ in range(N):
+    e = int(input())
+    wntList.append(e)
+wPointer = 0
 
-tmpans = (v-a)/(a-b)
-tmpans = math.ceil(tmpans)
-print((tmpans+1))
+producer = 1
+stack = []
+ans = []
+while True:
+    #print(producer, stack, ans)
+    # 갈망
+    if stack and stack[-1] == wntList[wPointer]:
+        ans.append('-')
+        stack.pop()
+        wPointer += 1
+        continue
+
+    # 충족
+    if producer <= N:
+        stack.append(producer)
+        producer += 1
+        ans.append('+')
+        continue
+    # 욕먕
+    if producer > N and stack and stack[-1] != wntList[wPointer]:
+        break
+    if producer > N and len(stack) == 0:
+        break
+
+if stack:
+    print('NO')
+else:
+    for a in ans:
+        print(a)

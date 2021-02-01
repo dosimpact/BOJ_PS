6c82d3e0930f2de8e5ef9d06e5b561a390142ab9
DosImpact <ypd03008@gmail.com>
2021-02-01 23:01:29 +0900

    [수학] 소수 구하기

--- env_python/mainpy.py
+++ env_python/mainpy.py
@@ -1,23 +1,23 @@
 
-# 문자열로 구성된 리스트 strings와, 정수 n이 주어졌을 때, 각 문자열의 인덱스 n번째 글자를 기준으로 오름차순 정렬
 
-# 인덱스 1의 문자가 같은 문자열이 여럿 일 경우, 사전순으로 앞선 문자열이 앞쪽에 위치합니다.
+def solution(n: int):
+    # 토스 체
+    check = [-1 for i in range(0, n+1)]
+    pc = 0  # 소수의 갯수
+    pn = []  # 소수의 배열
+    # -1 소수인것 체크.
+    for i in range(2, n+1):  # 2...n 까지 검사
+        if check[i] == -1:  # 소수임
+            pc += 1
+            pn.append(i)
+            # 3*3 = 9 , 12 , 15 ... n까지 제거
+            for j in range(i*i, n+1, i):  # 2*2 =4 ,6 8, ...,n 까지 제거
+                check[j] = 1
+    return pc
 
-import collections
-import itertools
 
-
-def solution(strings, n):
-    strings = sorted(strings, key=lambda x: (x[n], x))
-    return strings
-
-
-pool = ['a', 'b', 'c']
-
-# [('a', 'b', 'c'), ('a', 'c', 'b'), ('b', 'a', 'c'), ('b', 'c', 'a'), ('c', 'a', 'b'), ('c', 'b', 'a')]
-print(list(itertools.permutations(pool)))
-
-# ['abc', 'acb', 'bac', 'bca', 'cab', 'cba']
-print(list(map(lambda x: "".join(x), itertools.permutations(pool))))
-# ['abc', 'acb', 'bac', 'bca', 'cab', 'cba']
-print(list(map("".join, itertools.permutations(pool))))
+print(solution(1))
+print(solution(2))
+print(solution(5))  # 2 3  5
+print(solution(10))
+print(solution(1000))

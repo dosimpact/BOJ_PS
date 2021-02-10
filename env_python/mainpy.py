576105e2a5339db9965c27d7ea99ab6d0bc1797c
DosImpact <ypd03008@gmail.com>
2021-02-10 17:38:59 +0900

    15988

--- env_python/mainpy.py
+++ env_python/mainpy.py
@@ -1,5 +1,3 @@
-# https://www.acmicpc.net/problem/11723
-
 import sys
 import math
 from typing import *
@@ -8,28 +6,14 @@ import itertools
 input = sys.stdin.readline
 sys.setrecursionlimit(10**6)
 
+DIVIDER = 10007
 
-M = int(input())
-S = 0
+d = [0 for _ in range(1001)]
+d[1] = 1
+d[2] = 3
+for i in range(3, 1001):
+    d[i] = d[i-1]+d[i-2]*2
+    d[i] = d[i] % DIVIDER
 
-for _ in range(M):
-    com = input().split()
-    if com[0] == "add":  # 추가 , OR 로
-        x = int(com[1])
-        S = S | (1 << x)
-    if com[0] == "remove":  # 제거, AND + NOT
-        x = int(com[1])
-        S = S & ~(1 << x)
-    if com[0] == "check":  # exits,  AND
-        x = int(com[1])
-        res = S & (1 << x)
-        print(1) if res else print(0)
-    if com[0] == "toggle":  # XOR 연산자 이용
-        x = int(com[1])
-        S = S ^ (1 << x)
-    if com[0] == "all":  # 1~20 = 0~20까지로 가정 , 21개 , N =21
-        # S = S | (2**20 - 1)
-        S = S | ((1 << 21) - 1)
-    if com[0] == "empty":
-        S = 0
-    # print(f"S : {S} {bin(S)}")
+N = int(input())
+print(d[N])
--- env_python/mainpy4.py
+++ env_python/mainpy4.py
@@ -6,28 +6,18 @@ import itertools
 input = sys.stdin.readline
 sys.setrecursionlimit(10**6)
 
-N = int(input())  # 7일, =>
-T: List[int] = []
-P: List[int] = []
 
-for i in range(N):
-    t, p = map(int, input().split())
-    T.append(t)
-    P.append(p)
+X = int(input())
 
-maxVal = 0
-
-
-def go(day: int, earn: int):  # 현재 날짜, 벌어들인 수익
-    global maxVal
-    if day == N:  # 퇴사 날짜보다 길어진 경우 , = 상담할것이 없는경우
-        maxVal = max(earn, maxVal)
-        return
-    if day > N:
-        return
-    go(day+T[day], earn+P[day])  # 더 상담이 가능한 경우 - 할래, 안할래
-    go(day+1, earn)
+d = [0 for _ in range(10**6+1)]
+d[2] = 1
+d[3] = 1
+for i in range(4, 10**6+1):
+    d[i] = d[i-1]+1
+    if i % 2 == 0:
+        d[i] = min(d[i], d[i//2]+1)
+    if i % 3 == 0:
+        d[i] = min(d[i], d[i//3]+1)
 
 
-go(0, 0)
-print(maxVal)
+print(d[X])

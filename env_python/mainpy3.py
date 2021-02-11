8e58b6edec620845ed947a28e9402dc785027789
DosImpact <ypd03008@gmail.com>
2021-02-12 01:44:22 +0900

    redir

--- env_python/mainpy3.py
+++ env_python/mainpy3.py
@@ -8,40 +8,12 @@ sys.setrecursionlimit(10**6)
 
 
 T = int(input())
-for i in range(T):
-    isEndWith_1 = False
-    isPtn = True
-    ss: str = input().strip()
-    ss = ss+"E"
-    while not ss.startswith("E"):
-        # print("ss:", ss, " ", len(ss))
-        # 01
-        if ss.startswith("01"):  # 01 제거
-            ss = ss[2:]
-            isEndWith_1 = False
-        # 100+1+
-        elif ss.startswith("100") or (ss.startswith("00") and isEndWith_1):  # 100 제거
 
-            if isEndWith_1:
-                ss = ss[2:]
-            else:
-                ss = ss[3:]
-            isEndWith_1 = False
-
-            while ss[0] == "0":  # 0+제거
-                ss = ss[1:]
-            if (not ss) or ss[0] != "1":
-                isPtn = False
-                break
-            if ss[0] == "1":
-                ss = ss[1:]
-            while ss[0] == "1":
-                ss = ss[1:]
-                isEndWith_1 = True
-        else:
-            isPtn = False
-            break
-    if isPtn:
-        print("YES")
-    else:
-        print("NO")
+for _ in range(T):
+    N, M = map(int, input().split())
+    graph = [[]for _ in range(N+1)]
+    for __ in range(M):
+        u, v = map(int, input().split())
+        graph[u].append(v)
+        graph[v].append(u)
+    print(N-1)

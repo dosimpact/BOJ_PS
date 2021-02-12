1c709510f73224faa4913bbaeb4f0aa43a2d2d97
DosImpact <ypd03008@gmail.com>
2021-02-12 21:17:08 +0900

    4386

--- env_python/test1.py
+++ env_python/test1.py
@@ -1,14 +1,9 @@
-import heapq
-
-
-arr = [-4, -1, -5, -3, -8, -6, -3]
-print(arr)
-heapq.heapify(arr)
-print(arr)
-print(arr[0])
-
-
-heapq.heappush(arr, 2)
-print(arr)
-while arr:
-    print(heapq.heappop(arr))
+print(ord('a'))
+print(ord('A'))
+print(ord('1'))
+data = ["A910", "Z321"]
+print(data)
+data.sort()
+print(data)
+data.sort(reverse=True)
+print(data)

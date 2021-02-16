925ab90d8d7bcebc16cd22dcf64efb19de33a47a
DosImpact <ypd03008@gmail.com>
2021-02-16 22:42:20 +0900

    11060

--- env_python/test1.py
+++ env_python/test1.py
@@ -1,1 +1,15 @@
-print("hello"[:5])
+import sys
+import math
+import re
+from typing import *
+
+input = sys.stdin.readline
+
+sys.setrecursionlimit(10**6)
+
+
+d = [math.inf]
+print(d)
+print(d[0] < 10)
+print(d[0] < 102)
+print(d[0] < 103)

c11855b96b981b6ec1ebaf4f773c55a23800ba80
DosImpact <ypd03008@gmail.com>
2021-02-18 00:49:45 +0900

    1744

--- env_python/mainpy3.py
+++ env_python/mainpy3.py
@@ -7,16 +7,42 @@ input = sys.stdin.readline
 
 sys.setrecursionlimit(10**6)
 
+D = dict()
+A = input().strip()
+B = input().strip()
+if len(A) > len(B):
+    B, A = A, B
 
-N = int(input())
-Cost = [list(map(int, input().split())) for _ in range(N)]  # [N번집][R,g,b] 비용
-D = [[0 for _ in range(3)] for _ in range(N)]  # [N번집][R,g,b] 최소값
-for i in range(3):
-    D[0][i] = Cost[0][i]
+for i in range(1, len(A)):
+    # 길이가 i 인 문자열에서 공통인 부분 찾기
+    for j in range(0, len(A)):
+        res = B.find(A[j])
+        if res != -1:
+            if i not in D:
+                D[i] = []
+            D[i].append([A[j+1:], B[res+1:]])
+    break
 
-for i in range(1, N):
-    D[i][0] = min(D[i-1][1], D[i-1][2]) + Cost[i][0]
-    D[i][1] = min(D[i-1][0], D[i-1][2]) + Cost[i][1]
-    D[i][2] = min(D[i-1][0], D[i-1][1]) + Cost[i][2]
 
-print(min(D[N-1]))
+ans = 0
+for i in range(2, len(A)):
+    D[i] = []  # 길이 2를 만들어야함
+    # 길이 3을 만들면 1을 지워도 된
+    if i >= 3:
+        del(D[i-2])
+    DDown = D[i-1]
+    for pair in DDown:
+        a, b = pair
+        if (not a)or (not b):
+            continue
+        for j in range(0, len(a)):  # i-1 부터 검색
+            res = b.find(a[j])
+            if res != -1:
+                D[i].append([a[j+1:], b[res+1:]])
+                ans = i
+print(ans)
+
+"""
+AWOFNOWEFNOEWNFEWONFEWOFNAOEWFONEWFNOANAWGNWENGNGNWEGNWEGNEGWENGNEGNGNWEGNWENGENWGNAWGNWENGNGNWEGNWEGNEGWENGNEGNGNWEGNWENGENWGNAWGNWENGNGNWEGNWEGNEGWENGNEGNGNWEGNWENGENWGNAWGNWENGNGNWEGNWEGNEGWENGNEGNGNWEGNWENGENWGNAWGNWENGNGNWEGNWEGNEGWENGNEGNGNWEGNWENGENWG
+NAWGNWENGNGNWEGNWEGNEGWENGNEGNGNWEGNWENGENWGNAWGNWENGNGNWEGNWEGNEGWENGNEGNGNWEGNWENGENWGNAWGNWENGNGNWEGNWEGNEGWENGNEGNGNWEGNWENGENWGNAWGNWENGNGNWEGNWEGNEGWENGNEGNGNWEGNWENGENWG
+"""
--- env_python/mainpy4.py
+++ env_python/mainpy4.py
@@ -7,14 +7,16 @@ input = sys.stdin.readline
 
 sys.setrecursionlimit(10**6)
 
+A = input().strip()
+B = input().strip()
+D = [[0 for _ in range(len(B)+1)] for _ in range(len(A)+1)]  # [0,0] 계열 사용 X
 
-N = int(input())
-D = [[0 for _ in range(3)] for _ in range(N)]  # [N번집][R,g,b] 최소값
-D[0][0], D[0][1], D[0][2] = 1, 1, 1
-DIVER = 9901
-for i in range(1, N):
-    D[i][0] = D[i-1][0] + D[i-1][1]+D[i-1][2]
-    D[i][1] = D[i-1][0] + D[i-1][2]
-    D[i][2] = D[i-1][0] + D[i-1][1]
-    D[i][0], D[i][1], D[i][2] = D[i][0] % DIVER, D[i][1] % DIVER, D[i][2] % DIVER
-print(sum(D[N-1]) % DIVER)
+for i in range(len(A)):
+    for j in range(len(B)):
+        if A[i] == B[j]:  # 같은 경우
+            D[i+1][j+1] = D[i][j]+1
+            continue
+        else:  # 다른 경우
+            D[i+1][j+1] = max(D[i+1][j], D[i][j+1])
+
+print(D[len(A)][len(B)])

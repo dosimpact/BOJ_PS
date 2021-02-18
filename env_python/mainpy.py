498fdc227e1f1ef4880b54ae6b35559fe417c01e
dosimpact <ypd03008@gmail.com>
2021-02-18 19:58:15 +0900

    9252

--- env_python/mainpy.py
+++ env_python/mainpy.py
@@ -1,21 +1,39 @@
 import sys
 import math
 import re
+import heapq
 from typing import *
 
 input = sys.stdin.readline
 
 sys.setrecursionlimit(10 ** 6)
 
-N = int(input())
-data = list(map(int, input().split()))
+V, E = map(int, input().split())
+K = int(input())
+graph = [[] for _ in range(V + 1)]  # 0 un use
+for _ in range(E):
+    u, v, w = map(int, input().split())
+    graph[u].append((v, w))
 
-d = [0 for _ in range(N)]
+check = [False for _ in range(V + 1)]
+dist = [math.inf for _ in range(V + 1)]
+pq = [(0, K)]
+heapq.heapify(pq)  # 거리 , 다음 노드 정보
+dist[K] = 0
 
-for i in range(len(data)):
-    d[i] = data[i]
-    for j in range(0, i):
-        if data[j] < data[i]:
-            d[i] = max(d[j] + data[i], d[i])
+while pq:
+    now_w, now = heapq.heappop(pq)  # 현재 노드에서 탐색
+    if check[now]:
+        continue
+    check[now] = True
+    for nxt, nxt_w in graph[now]:  # 다음노드로 가는 거리 계산 후 갱신 , 엔큐
+        if dist[nxt] > dist[now] + nxt_w:
+            dist[nxt] = dist[now] + nxt_w
+            heapq.heappush(pq, (dist[nxt], nxt)) # dist중에서 최소 + 아직 방문하지 않은 점이 다음 목표임
+            # heapq.heappush(pq, (nxt_w, nxt))
 
-print(max(d))
+for d in dist[1:]:
+    if d == math.inf:
+        print("INF")
+    else:
+        print(d)
--- env_python/mainpy5.py
+++ env_python/mainpy5.py
@@ -1,24 +1,44 @@
 import sys
-import math
-import re
 from typing import *
 
 input = sys.stdin.readline
 
-sys.setrecursionlimit(10**6)
 
-N = int(input())
-data = [list(map(int, input().split())) for _ in range(N)]  # [0][0]
-D = [[0 for _ in range(i+1)] for i in range(N)]
-D[0][0] = data[0][0]
+N: int = int(input())
+graph: List[List[str]] = [list(input().strip()) for _ in range(N)]  # 인접리스트
+ans = []
 
-for i in range(1, N):  # 1층부터
-    # 0 ,1 가능
-    for j in range(0, i+1):  # i층의 j번째 노드
-        left, right = 0, 0
-        if j != 0:
-            left = D[i-1][j-1]
-        if j != i:
-            right = D[i-1][j]
-        D[i][j] = max(left, right) + data[i][j]
-print(max(D[N-1]))
+
+for i in range(N):
+    check = [-1 for _ in range(N)]
+    check[i] = 0  # 나는 0 , 1 , 2촌까지 검색
+    q = [i]
+    data = 0
+    while q:
+        now = q.pop(0)
+        for nxt in range(N):
+            if graph[now][nxt] == "Y" and check[nxt] == -1:
+                if check[now] <= 1:
+                    check[nxt] = check[now] + 1
+                    q.append(nxt)
+                    data += 1
+    ans.append(data)
+
+print(max(ans))
+
+"""
+4
+NYNN
+YNYN
+NYNY
+NNYN
+
+
+6
+NYYNYN
+YNYNNN
+YYNYNN
+NNYNNN
+YNNNNY
+NNNNYN
+"""

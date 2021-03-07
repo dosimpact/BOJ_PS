e5738231b3e0ba6043c46540c407d2426ef5cb89
DosImpact <ypd03008@gmail.com>
2021-03-07 17:37:20 +0900

    2151

--- env_python/mainpy4.py
+++ env_python/mainpy4.py
@@ -1,37 +1,137 @@
-import sys
-import heapq
-import re
-import math
-from collections import deque
-from typing import *
-from math import ceil
+# import sys
+# import heapq
+# import re
+# import math
+# from collections import deque
+# from typing import *
+# from math import ceil
 
-input = sys.stdin.readline
 
-sys.setrecursionlimit(10**6)
+# 첫번쨰거울에서 시작 , (방향,위치,튕긴거울수) - whle문 돌려,
+N = int(input())
+graph = [list(input()) for _ in range(N)]
+sx, sy, isend = 0, 0, False
+for i in range(N):
+    for j in range(N):
+        if graph[i][j] == "#":
+            sx, sy = i, j
+            isend = True
+            break
+    if isend:
+        break
+dx, dy = [-1, 0, 1, 0], [0, 1, 0, -1]
+q = [(0, sx, sy, 0), (0, sx, sy, 1), (0, sx, sy, 2),
+     (0, sx, sy, 3)]  # 튕긴 거울의 수|좌표|현재 나가는 방향
 
-# 1..N 문제 - 난이도 순서
-# 가능한 쉬운문제 부터, 먼저푸는것이 좋은 문제
-# q 순위
-# 1. 위상정렬 조건 -> inDegree 0 조건
-# 2. 문제 난이도 조건  -> 우선순위 큐
 
-N, M = map(int, input().split())
-graph = [[] for _ in range(N+1)]
-inDeg = [0 for _ in range(N+1)]  # 0 사용 X
-for _ in range(M):
-    u, v = map(int, input().split())  # 4->2
-    graph[u].append(v)
-    inDeg[v] += 1
-pq = []
-for i in range(1, N+1):  # 1번 노드 ~ 4번 노드
-    if inDeg[i] == 0:
-        heapq.heappush(pq, i)
+def inRange(x: int, y: int):
+    return x >= 0 and y >= 0 and x < N and y < N
 
-while pq:
-    now = heapq.heappop(pq)
-    print(now, end=" ")
-    for nxt in graph[now]:
-        inDeg[nxt] -= 1
-        if inDeg[nxt] == 0:
-            heapq.heappush(pq, nxt)
+
+# 빈공간 - 계속 , 벽,out of range - stop, 거울 - 양갈래로 직진 큐 생성
+# 무한 루프를 도는 빛이 있으므로 최단거리로 이동을 살펴야한다.
+while q:
+    w, x, y, di = q.pop(0)
+    while True:  # 빛의 이동
+        nx, ny = x+dx[di], y+dy[di]
+        if not inRange(nx, ny):
+            break
+        if graph[nx][ny] == "*":
+            break
+        if graph[nx][ny] == "#":
+            # print(f"END : {nx,ny,w,di}")
+            if not(nx == sx and ny == sy):
+                print(w)
+                exit(0)
+        if graph[nx][ny] == "!":
+            q.append((w+1, nx, ny, (di+1) % 4))
+            q.append((w+1, nx, ny, (di-1) % 4))
+        x, y = nx, ny
+
+"""
+5
+***#*
+*.!.*
+*!.!*
+*.!.*
+*#***
+>2
+
+5
+*#***
+*.!.*
+*!.!*
+*.!.*
+*#***
+>
+
+5
+*****
+*!!.#
+*!.!*
+*!!.*
+*#***
+>1
+
+5
+.....
+!...#
+.....
+.*...
+!#...
+>2
+
+5
+.#...
+!!...
+.....
+.*...
+!#...
+>3
+
+5
+.#...
+.!..!
+.....
+.!..!
+.#...
+>4
+
+9
+.!*......
+..!.!*!.!
+#.!*.*.*.
+!!.*!.!*.
+.*.......
+.#......!
+.........
+.........
+!.......!
+>3
+
+❌
+- 비친 문이 알고보니 자기자신..
+7
+!!..!.#
+*.....*
+.!..!..
+!!*.!.!
+.*!!.*!
+...!!..
+!*#****
+>5
+
+❌
+10
+****#*****
+!!..!.!..!
+......*...
+......!..!
+*!....!..*
+......!..!
+.!....!...
+...!*.*...
+!..!!.!..!
+****#*****
+
+"""
--- env_python/test1.py
+++ env_python/test1.py
@@ -1,105 +1,60 @@
-
-
-# 세그먼트 트리
-
-# 기능
-# 1.세그먼트 트리 만들기 (init)
-# 2.update
-# 3.sum
-
-# 탐색
-# - 루트 노드는 인덱스 1부터 시작
-# 노드의 인덱스가 K 라면 왼쪽은 K*2 , 오른쪽은 K*2+1 이다.
-
-# 시간 복잡도
-# 숫자 검색 =  logN
-# 숫자 업데이트 = logN
-
-# 공간 복잡도
-# (최선) 3**2 < 12 < 4**2 라면 > (4**2) * 2 가 트리 크기
-# (차선) tree배열 크기 = N*4
-
-import math
+from collections import deque
 import sys
-sys.setrecursionlimit(10**6)
-# N = 12 (12 숫자의 구간합을  구해보자)
-arr = [3, 5, 6, 7, 2, 9, 4, 5, 2, 8, 1, 5]
-h = int(math.ceil(math.sqrt(len(arr))))  # log12 = 3.xx -> 4
-tree_size = (1 << (h+1))  # 2**5
-tree = [0 for _ in range(tree_size+1)]
-print("height", h)
-print("tree_size", tree_size)
 
-# tree의 idx는 node가 담당, arr의 idx는 start,end 이다.
+input = sys.stdin.readline
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
 
 
-def seg_init(node: int, start: int, end: int):  # [start,end]를 초기화 하고 싶다.
-    # print(f"node,start,end : {node,start,end}")
-    if start == end:
-        tree[node] = arr[start]
-        return tree[node]
-    mid = (start+end)//2
-    tree[node] = seg_init(node*2, start, mid) + seg_init(node*2+1, mid+1, end)
-    return tree[node]
+def bfs(x, y, dir):
+    q.append([x, y, dir])
+    c[x][y][dir] = 1
+    ans = []
+    while q:
+        x, y, dir = q.popleft()
+        nx = x + dx[dir]
+        ny = y + dy[dir]
+        if 0 <= nx < n and 0 <= ny < n:
+            if not c[nx][ny][dir] or c[nx][ny][dir] > c[x][y][dir]:
+                if a[nx][ny] != '*':
+                    c[nx][ny][dir] = c[x][y][dir]
+                    if nx == fx and ny == fy:
+                        ans.append(c[nx][ny][dir])
+                        continue
+                    q.append([nx, ny, dir])
+                    if a[nx][ny] == '!':
+                        turn(nx, ny, dir)
+
+    print(min(ans)-1)
 
 
-seg_init(1, 0, 11)
-print(tree)
-# [0, 57, 32, 25, 14, 18, 11, 14, 8, 6, 9, 9, 9, 2, 9, 5, 3, 5, 0, 0, 7, 2, 0, 0, 4, 5, 0, 0, 8, 1, 0, 0, 0]
-
-# 재귀변수 node,start,end
-# idx의 변수를 바꾸었고, 그 차이는 diff이다.
-
-
-def seg_update(node: int, start: int, end: int, idx: int, diff: int):
-    # 특정 leaf 노드르 값을 바꾸는 경우 - 범위에 관련된 노드가 update
-    if not(start <= idx and idx <= end):  # basecase  - out of idx
-        return
-    tree[node] += diff
-    if start == end:  # basecase - leaf node
-        return
-    mid = (start+end)//2
-    seg_update(node*2, start, mid, idx, diff)
-    seg_update(node*2+1, mid+1, end, idx, diff)
-    return
+def turn(x, y, dir):
+    ndir = [(dir+1) % 4, (dir+3) % 4]
+    for d in ndir:
+        if not c[x][y][d] or c[x][y][d] > c[x][y][dir] + 1:
+            c[x][y][d] = c[x][y][dir] + 1
+            q.append([x, y, d])
 
 
-arr[0] = 10
-seg_update(1, 0, 11, 0, 7)  # 0~11을 가지는 root 노드부터 전파,( 0번을 +7 했다.)
-print(tree)
-"""
-[0, 57, 32, 25, 14, 18, 11, 14, 8, 6, 9, 9, 9, 2, 9, 5, 3, 5, 0, 0, 7, 2, 0, 0, 4, 5, 0, 0, 8, 1, 0, 0, 0]
-[0, 64, 39, 25, 21, 18, 11, 14, 15, 6, 9, 9, 9, 2, 9, 5, 10, 5, 0, 0, 7, 2, 0, 0, 4, 5, 0, 0, 8, 1, 0, 0, 0]
-"""
+n = int(input())
+q = deque()
+c = [[[0]*4 for _ in range(n)] for _ in range(n)]
 
-# node-start-end 에서
-# 쿼리 LR 의 합을 구하고자 한다.
-# 특정 노드 -- 쿼리 :
-"""
-# 1. 전혀 상관없는 경우
-# 2. 해당 노드에서 알 수 있는 정보보다, 요청사항이 많은경우
-    - 우선 노드 정보를 다 주자.
-# 3. 해당 노드에서 알 수 있는 정보보다, 요청사항이 적은 경우
-    - 다음 노드들에게 맡기자.
-"""
+a, temp = [], []
+for i in range(n):
+    row = list(input().strip())
+    a.append(row)
+    for j in range(n):
+        if row[j] == '#':
+            temp.extend([i, j])
+sx, sy, fx, fy = temp
 
+for i in range(4):
+    nx = sx + dx[i]
+    ny = sy + dy[i]
+    if 0 <= nx < n and 0 <= ny < n:
+        if a[nx][ny] != '*':
+            dir = i
+            break
 
-def seg_sum(node: int, start: int, end: int, L: int, R: int):
-    if (end < L)or(R < start):  # basecase - 1
-        return 0
-    if L <= start and end <= R:  # basecase - 2
-        return tree[node]
-    mid = (start + end)//2  # basecase - 3
-    return seg_sum(node*2, start, mid, L, R) + seg_sum(node*2+1, mid+1, end, L, R)
-
-
-# arr = [3+7, 5, 6, 7, 2, 9, 4, 5, 2, 8, 1, 5]
-print(f"[0,0]의 합 : {seg_sum(1,0,11,0,0)}")
-print(f"[0,1]의 합 : {seg_sum(1,0,11,0,1)}")
-print(f"[0,2]의 합 : {seg_sum(1,0,11,0,2)}")
-print(f"[0,3]의 합 : {seg_sum(1,0,11,0,3)}")
-
-print(f"[1,8]의 합 : {seg_sum(1,0,11,1,8)}")
-print(f"[2,8]의 합 : {seg_sum(1,0,11,2,8)}")
-print(f"[3,8]의 합 : {seg_sum(1,0,11,3,8)}")
-print(f"[4,8]의 합 : {seg_sum(1,0,11,4,8)}")
+bfs(sx, sy, dir)

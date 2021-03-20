ef2fa476b18753c5cc1f7257c962a63db9bb233e
DosImpact <ypd03008@gmail.com>
2021-03-20 15:46:10 +0900

    scofe21

--- env_python/test1.py
+++ env_python/test1.py
@@ -1,14 +1,2 @@
-<<<<<<< HEAD
-
-
-print((1, 2)+(3, 4)
-=======
-from functools import reduce
-
-# sum
-print(reduce(lambda x, y: x + y, [2, 3, 4]))
-# product
-print(reduce(lambda x, y: x * y, [2, 3, 4]))
-# "".join
-print(reduce(lambda x, y: x * 10 + y, [2, 3, 4]))
->>>>>>> 7bfffbf4e25e7e4d2d2bcec6f88b45072b8caae1
+for i, (dx, dy) in enumerate(zip([0, -1, 1], [1, 0, 0])):
+        print(i, (dx, dy))

9f2242656c6d828d96032f1d96af2ce646bf2a2f
DosImpact <ypd03008@gmail.com>
2021-03-24 23:28:10 +0900

    14442

--- env_python/mainpy2.py
+++ env_python/mainpy2.py
@@ -1,63 +1,72 @@
-import sys
-import math
-from collections import deque
-
-input = sys.stdin.readline
-sys.setrecursionlimit(10 ** 6)
 
 
-# 모든 사진 틀은 비어 있음
-# 추천시 게시,
-# 비어있는 사진 틀이 없다 - 추천받은 횟수 가장 적은 학생 사진 삭제
-# 동점자 ? - 가장 오래된 사람 제거
-# 이미 게시된 경우 - 추천 횟수 증가
-# 삭제 되는 경우 추천 횟수 리셋
-# 최종 후보 오름차 출력
 
-# 추천수,게시순서,학생번호
-N = int(input())
-M = int(input())
-data = list(map(int, input().split()))
-d = dict()
+# 세그먼트 트리
+# 4배 메모리,루트1, node,start,end, 원본 업데이트 필요
+# init,query,update
 
-for idx, t in enumerate(data):
-    if t in d:  # 있음 ❌ 있는것을 먼저 체크
-        d[t][0] += 1
-    elif len(d.keys()) < N:  # 빈 액자
-        d[t] = [1, idx]
-    else:  # 제거
-        wridx = sorted(zip(d.values(), d.keys()),
-                       key=lambda x: (x[0][0], x[0][1]))
-        # print(f"wridx {wridx}")
-        del d[wridx[0][1]]
-        d[t] = [1, idx]
+arr = []
+tree = []
 
-print(*sorted(d.keys()))
+
+def init(node, start, end):
+    if start == end:
+        tree[node] = arr[start]
+        return tree[node]
+    mid = (start+end)//2
+    tree[node] = init(node*2, start, mid) + init(node*2+1, mid+1, end)
+    return tree[node]
+
+# 구간 합에 대한 요청
+
+
+def query(node, start, end, L, R):
+    # 요청이 과다,걸처있는 범위,상관없는 범위
+    if R < start or end < L:
+        return 0
+    if L <= start and end <= R:
+        return tree[node]
+    mid = (start+end)//2
+    res = query(node*2, start, mid, L, R) + query(node*2+1, mid+1, end, L, R)
+    return res
+
+
+def update(node, start, end, diff, idx):
+    # 상관없는 범위 | 업데이트 | 전파 가능?
+    if idx < start or end < idx:
+        return
+    tree[node] += diff
+    if start == end:
+        return
+    mid = (start+end)//2
+    update(node*2, start, mid, diff, idx)
+    update(node*2+1, mid+1, end, diff, idx)
+
+
+N, M, K = map(int, input().split())
+arr = [int(input()) for _ in range(N)]
+tree = [0 for _ in range(len(arr)*4)]
+init(1, 0, len(arr)-1)
+for _ in range(M+K):
+    a, b, c = map(int, input().split())
+    if a == 1:
+        b -= 1
+        diff = c - arr[b]
+        arr[b] = c
+        update(1, 0, len(arr)-1, diff, b)
+    else:
+        # b,c 합 출력
+        print(query(1, 0, len(arr)-1, b-1, c-1))
 
 """
-❌
+5 2 2
+1
+2
 3
-7
-1 2 2 3 3 3 4 5 6
-
-3
-9
-2 1 4 3 5 6 2 7 2
-
-3
-6
-1 2 3 1 2 3
->1 2 3
-
-3
-3
-1 1 1
->1
-
-3
-7
-1 2 3 3 2 1 4 5 6
->2 3 6
-
-
+4
+5
+1 3 6
+2 2 5
+1 5 2
+2 3 5
 """

de8f7c1a6bf203d81f8da7d9484d5f7ec79e4100
DosImpact <ypd03008@gmail.com>
2021-04-05 10:39:13 +0900

    setting node.js + ts

--- env_python/mainpy.py
+++ env_python/mainpy.py
@@ -1,34 +1,22 @@
-# eg) 인공지능, 데이터 마이닝 수업의 점수 합계를 구해라
-# 1. 인공지능 새로운 학생 들어옴
-# 2. 이때, 데이터마이닝 수업에 index 얼라인을 맞추고 합계
-# 3. 새로운 학생 추가 , ffill 이용
+from sys import stdin, setrecursionlimit
+from collections import deque
 
-lecture_index = ['321600', '321601', '321602']
-lecture_colums = ['midterm', 'final']
+input = stdin.readline
+setrecursionlimit(10 ** 6)
 
-df_ai = pd.DataFrame([[50, 50], [45, 40], [30, 20]],
-                     index=lecture_index,
-                     columns=lecture_colums)
-df_da = pd.DataFrame({
-    lecture_colums[0]: [50, 50, 50],
-    lecture_colums[1]: [10, 20, 15]
-}, index=lecture_index)
 
-df_ai.loc['321603'] = [50, 50]  # 1
-df_da = df_da.reindex(df_ai.index).replace(np.nan, 0)
-df_sum = df_ai + df_da  # -> 2 출력
+# N개의 종이로 가린다.
+# M개 넘개 겹쳐야 안보임
 
-df_ai.loc['321604'] = [10, 10]
-# df_da.reindex(df_ai.index,fill_value="RE")
-df_da = df_da.reindex(df_ai.index, method="ffill")
-df_sum = df_ai + df_da  # -> 2 출력
+N, M = map(int, input().split())
+graph = [[0 for _ in range(100)] for _ in range(100)]
+for _ in range(N):
+    x1, y1, x2, y2 = map(lambda x: int(x)-1, input().split())
+    for i in range(x1, x2+1):
+        for j in range(y1, y2+1):
+            graph[i][j] += 1
 
-df_sum
-"""
-midterm final
-321600 100.000 60.000
-321601 95.000 60.000
-321602 80.000 35.000
-321603 50.000 50.000
-321604 10.000 10.000
-"""
+cnt = 0
+for row in graph:
+    cnt += len(list(filter(lambda x: x > M, row)))
+print(cnt)
---
+++ env_ts/src/main1.js
@@ -0,0 +1,27 @@
+const TC = `
+5
+1 1
+2 3
+3 4
+9 8
+5 2
+`;
+const stdin =
+  process.platform === "linux"
+    ? require("fs").readFileSync("/dev/stdin").toString().split("\n")
+    : TC.trim().split("\n");
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+let t = Number(input());
+while (t--) {
+  console.log(
+    input()
+      .split(" ")
+      .map(Number)
+      .reduce((a, c) => a + c)
+  );
+}
